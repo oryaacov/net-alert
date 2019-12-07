d51036e0d2eeb7e2093187db3c712ec401574cbc
Or Yaacov <oryaacov@live.com>
2019-12-07 19:25:27 +0200

    styiling

--- net-alert-frontend/src/app/app.module.ts
+++ net-alert-frontend/src/app/app.module.ts
@@ -1,25 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {StoreDevtoolsModule} from '@ngrx/store-devtools'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
 import { EffectsModule } from '@ngrx/effects';
 import { NetAlertEffects } from './root-store/net-alert-store/net-alert.effects';
 import { ProfileComponent } from './components/profile/profile.component';
 import { ProfileContainerComponent } from './containers/profile-container/profile-container.component';
 import { RootStoreModule } from './root-store/root-store.module';
 import { environment } from 'src/environments/environment';
-import { reducers} from './root-store/root-state';
+import { reducers } from './root-store/root-state';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxLoadingModule } from 'ngx-loading';
-import { MatFormFieldModule, MatInputModule, MatCheckboxModule, MatCardModule, MatListModule, MatTableModule, MatDialogModule, MatTabsModule, MatIconModule } from '@angular/material';
+import { MatFormFieldModule, MatInputModule, MatCheckboxModule, MatCardModule, MatListModule, MatTableModule, MatDialogModule, MatTabsModule, MatIconModule, MatToolbarModule, MatGridListModule } from '@angular/material';
 import { ToastrModule } from 'ngx-toastr';
 import { OwnerComponent } from './components/owner/owner.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MyNetworkComponent } from './components/my-network/my-network.component';
 import { DialogBoxComponent } from './dialogs/dialog-box/dialog-box.component';
+import { DragDropModule } from '@angular/cdk/drag-drop'
 @NgModule({
   declarations: [
     AppComponent,
@@ -32,8 +33,11 @@ import { DialogBoxComponent } from './dialogs/dialog-box/dialog-box.component';
   imports: [
     HttpClientModule,
     ReactiveFormsModule,
+    MatGridListModule,
+    MatToolbarModule,
     MatFormFieldModule,
     MatListModule,
+    DragDropModule,
     FormsModule,
     MatTabsModule,
     MatIconModule,
@@ -45,11 +49,11 @@ import { DialogBoxComponent } from './dialogs/dialog-box/dialog-box.component';
     MatCheckboxModule,
     MatInputModule,
     BrowserAnimationsModule,
-    ToastrModule.forRoot({timeOut:3000,positionClass:'toast-bottem-left'}),
+    ToastrModule.forRoot({ timeOut: 3000, positionClass: 'toast-bottem-left' }),
     NgxLoadingModule,
     AppRoutingModule,
     StoreModule.forRoot(reducers),
-     EffectsModule.forRoot([NetAlertEffects]),
+    EffectsModule.forRoot([NetAlertEffects]),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
       logOnly: environment.production, // Restrict extension to log-only mode
@@ -57,7 +61,7 @@ import { DialogBoxComponent } from './dialogs/dialog-box/dialog-box.component';
     RootStoreModule
   ],
   providers: [],
-  entryComponents:[DialogBoxComponent],
+  entryComponents: [DialogBoxComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
